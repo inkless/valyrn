ca75dea0b9545bcb394039225f490d071c83e60e
Guangda Zhang <zhangxiaoyu9350@gmail.com>
2015-07-02 14:24:30 -0700

    init

---
+++ app/main.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var app = require('app');
+var BrowserWindow = require('browser-window');
+var env = require('./vendor/valyrn/env_config');
+var devHelper = require('./vendor/valyrn/dev_helper');
+var windowStateKeeper = require('./vendor/valyrn/window_state');
+
+var mainWindow;
+
+// Preserver of the window size and position between app launches.
+var mainWindowState = windowStateKeeper('main', {
+    width: 1000,
+    height: 600
+});
+
+app.on('ready', function () {
+
+    mainWindow = new BrowserWindow({
+        x: mainWindowState.x,
+        y: mainWindowState.y,
+        width: mainWindowState.width,
+        height: mainWindowState.height
+    });
+
+    if (mainWindowState.isMaximized) {
+        mainWindow.maximize();
+    }
+
+    mainWindow.loadUrl('file://' + __dirname + '/app.html');
+
+    if (env.name === 'development') {
+        devHelper.setDevMenu();
+        mainWindow.openDevTools();
+    }
+
+    mainWindow.on('close', function () {
+        mainWindowState.saveState(mainWindow);
+    });
+});
+
+app.on('window-all-closed', function () {
+    app.quit();
+});
